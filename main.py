d9e313f7dbe93a8f37ebd7e4a68f35e577360c4b
aditya <addie4120@gmail.com>
2023-07-28 20:27:48 +0530

    added app descriptions, images and titles

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 st.title("Welcome To My Portfolio")
 st.header("About Me")
 
+st.markdown("""<hr style="height:3px;border:none;color:#FFFF00;background-color:#FFFF00;" /> """,
+            unsafe_allow_html=True)
+
 cl1, cl2 = st.columns(2)
 
 with cl1:
-    st.image("images_pw/photo.png")
+    st.image("images/adi.png", width=400)
 
 with cl2:
     st.title("Aditya Vikram Singh")
@@ -23,4 +27,30 @@ with cl2:
     """
     st.info(content)
 
-st.write("Below you can find some of the apps I have built. Feel free to contact me")
+st.markdown("""<hr style="height:3px;border:none;color:#FFFF00;background-color:#FFFF00;" /> """,
+            unsafe_allow_html=True)
+
+st.write("<h3><p style='font-family: Abril Fatface; color:Purple; font-size: 42px'>"
+         "Below you can find some of the apps I have built.</p></h3>",
+         unsafe_allow_html=True)
+
+st.markdown("""<hr style="height:3px;border:none;color:#FFFF00;background-color:#FFFF00;" />""",
+            unsafe_allow_html=True)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+cl3, emp_col,  cl4 = st.columns([1.5, 0.5, 1.5])
+
+with cl3:
+    for index, row in df[0:10].iterrows():
+        st.header(row["title"],)
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=400)
+        st.write(f"[SOURCE CODE]({row['url']})")
+
+with cl4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=400)
+        st.write(f"[SOURCE CODE]({row['url']})")
